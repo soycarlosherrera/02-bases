950c35b03a3b8edbcea6c7bc9c8b50f3380ac296
soycarlosherrera <carlos4505@gmail.com>
2025-04-04 14:39:19 -0500

    Add service character

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,13 +1,8 @@
 
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
@@ -16,24 +11,10 @@ interface Character {
 })
 export class DragonballSuperPageComponent {
 
-  name = signal('');
-  power = signal(0);
+  // constructor(
+  //   public dragonballService:DragonballService
+  // ){}
 
-  characters = signal<Character[]>([
-    {id: 1,name: 'Goku', power: 9001},
-    {id: 2,name: 'Vegeta', power: 8000},
-  ]);
-
-
-  addCharacter(character: Character){
-
-    this.characters.update((list)=>[...list,character]);
-
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  public dragonballService = inject(DragonballService);
 
 }
